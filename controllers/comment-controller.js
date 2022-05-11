681b043010b6b49e8ded4a7553413cb01d72627a
Hannah Bush <heb.rutgers@gmail.com>
2022-05-11 12:41:39 -0400

    pizza controllers updated to populate comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -23,26 +23,26 @@ const commentController = {
   },
 
   //remove comment
-  removeComment({params}, res) {
-    Comment.findByIdAndDelete({ _id: params.commentId})
-    .then(deletedComment => {
-      if(!deletedComment) {
-        return res.status(404).json({message: 'No comment with this id!'});
-      }
-      return Pizza.findByIdAndUpdate(
-        {_id: params.pizzaId},
-        {$pull: {comments: params.commentId}},
-        {new: true}
-      );
-    })
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => res.json(err));
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(deletedComment => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: 'No comment with this id!' });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   }
 };
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,6 @@ const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 //add prefix of /pizzas to routes created in pizzaroutes.js
-router.use('/pizzas', pizzaRoutes);
 router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
