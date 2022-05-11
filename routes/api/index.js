f9bb9df89e89fab91f0fb8b93e0e2bc5406e2b4b
Hannah Bush <heb.rutgers@gmail.com>
2022-05-11 10:22:13 -0400

    post and delete comment controllers added, index updated

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes')
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
 
 //add prefix of /pizzas to routes created in pizzaroutes.js
+router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
